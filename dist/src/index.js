587a89b874a94c2f0fbabdc3b705afacbf7ac50e
Redskull-127 <meerraja17@gmail.com>
2024-01-04 16:45:01 +0530

    Add socket.io server and message handling

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('Hello World!');
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const socket_io_1 = require("socket.io");
+const io = new socket_io_1.Server(process.env.NODE_ENV === "production" ? 80 : 8989, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+io.on("connection", socket => {
+    console.log("connected");
+    socket.on("send:message", (message) => {
+        console.log(message);
+        io.emit("receive:message", message);
+    });
+});
